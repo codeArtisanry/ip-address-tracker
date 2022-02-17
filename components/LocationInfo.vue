63686ab9acb4d652f425e0ccee8939900f32ca38
Payal17122000 <87559461+Payal17122000@users.noreply.github.com>
2022-02-17 19:16:16 +0530

    feat: Feature Added that shows Info when IP Address Given (#8)
    * feat: basic design added
    
    * feat: change file for conflict
    
    * feat: remove index.vue because unused and remove unused comments
    
    * feat: added feature that addshow location from IP address
    
    * fix: add new line and delete unused image
    
    * feat: plugin added
    
    * fix: meaningful naming
    
    * fix: naming changed
    
    * fix: timezone error solve
    
    * fix: vatsal's deleted file
    
    Co-authored-by: payal <payal@improwised.com>

--- components/LocationInfo.vue
+++ components/LocationInfo.vue
@@ -2,11 +2,27 @@
   <div class="container">
     <div class="second">
       <b-container class="bv-example-row">
-        <b-row class="col">
-          <b-col :ipaddress="ip">IP ADDRESS<br />{{ ipaddress }}</b-col>
-          <b-col>LOCATION</b-col>
-          <b-col>TIMEZONE</b-col>
-          <b-col>ISP</b-col>
+        <b-row class="column-s">
+          <b-col
+            ><div class="f1">IP ADDRESS</div>
+            <br /><b class="f2">{{ ip }}</b></b-col
+          >
+          <b-col
+            ><div class="f1">LOCATION</div>
+            <br /><b class="f2"
+              >{{ addresses.city }}, {{ addresses.country_code2 }}<br />{{
+                addresses.zipcode
+              }}</b
+            ></b-col
+          >
+          <b-col
+            ><div class="f1">TIMEZONE</div>
+            <br /><b class="f2">UTC -08:00</b></b-col
+          >
+          <b-col
+            ><div class="f1">ISP</div>
+            <br /><b class="f2">{{ addresses.isp }}</b></b-col
+          >
         </b-row>
       </b-container>
     </div>
@@ -14,5 +30,7 @@
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["addresses", "ip", "current_time"],
+};
 </script>
--- pages/payal/index.vue
+++ pages/payal/index.vue
@@ -1,27 +1,57 @@
 <template>
-  <div class="container d">
-    <h4 class="title"><b>IP Address Tracker</b></h4>
-    <form
-      id="form"
-      class="form-inline form"
-      method="post"
-      @submit.prevent="onclick"
-    >
-      <input
-        v-model="ip"
-        class="form-control mr-lg-6 textbox"
-        type="search"
-        placeholder="Search for any IP addresses or domain"
+  <div class="container">
+    <div class="d">
+      <h4 class="title"><b>IP Address Tracker</b></h4>
+      <form
+        id="form"
+        class="form-inline form"
+        method="post"
+        @submit.prevent="onclick"
+      >
+        <input
+          v-model="ip"
+          class="form-control mr-lg-6 textbox"
+          type="search"
+          placeholder="Search for any IP addresses or domain"
+          style="width: 300px"
+        />
+        <button
+          class="btn btn-dark my-2 my-sm-0 button-image"
+          type="submit"
+        ></button>
+      </form>
+      <LocationInfo
+        :addresses="addresses"
+        :ip="ip"
+        :current_time="current_time"
       />
-      <button
-        class="btn btn-dark my-2 my-sm-0 button-image"
-        type="submit"
-      ></button>
-    </form>
-    <LocationInfo />
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      addresses: [],
+      ip: "",
+      apikey: "ef3eeb9d020b49d19609313bc88ddb39",
+      lat_lng: [37.4224, -122.08421],
+      current_time: null,
+    };
+  },
+  methods: {
+    async onclick() {
+      const res = await this.$axios.$get(
+        "https://api.ipgeolocation.io/ipgeo?apiKey=" +
+          this.apikey +
+          "&ip=" +
+          this.ip
+      );
+      this.addresses = res;
+      this.current_time = this.addresses.time_zone.current_time;
+      this.$router.push("/payal");
+    },
+  },
+};
 </script>
