54c61c3f9ee20bf61c739a829c1c1404a7366a36
Vatsal <87218847+codesnail21@users.noreply.github.com>
2022-02-17 16:47:25 +0530

    feat: map component is staged. (#11)
    * feat: setup header & search bar
    
    * feat: init
    
    * feat: setup header & search bar
    
    * feat: modified css in ip info box
    
    * feat: map component is staged

---
+++ components/TheSearchBar.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <header>
+      <div class="search">
+        <input
+          type="text"
+          placeholder="Search for any IP Address"
+          :value="domain"
+          @input="$emit('input', $event.target.value)"
+        />
+        <button @click="fetchIpInfo">
+          <img :src="require('@/static/icon-arrow.svg')" alt="arrow icon" />
+        </button>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    domain: {
+      type: String,
+      default: "",
+    },
+    fetchIpInfo: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style>
+header {
+  display: flexbox;
+  position: relative;
+  text-align: center;
+}
+
+.search {
+  display: flex;
+  display: inline-flex;
+  position: relative;
+  z-index: 1;
+  bottom: 15em;
+}
+
+.search input,
+.search button {
+  border: 0;
+  outline: 0;
+  padding: 1.5em;
+}
+
+.search input {
+  width: 35em;
+  border-bottom-left-radius: 0.9em;
+  border-top-left-radius: 0.9em;
+}
+
+.search button {
+  background: black;
+  cursor: pointer;
+  border-top-right-radius: 0.9em;
+  border-bottom-right-radius: 0.9em;
+}
+</style>
