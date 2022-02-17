11a58ab903e0fa8db860301f3ad2dbb740b6c9fa
Vatsal <87218847+codesnail21@users.noreply.github.com>
2022-02-17 15:39:02 +0530

    Setup Header & Serch Bar (#5)
    * feat: setup header & search bar
    
    * feat: init
    
    * feat: setup header & search bar
    
    * feat: modified css in ip info box

---
+++ components/IpInfo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="ip-info-card">
+    <p>{{ title }}</p>
+    <h1>{{ value }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.ip-info-card {
+  flex: 1;
+  padding-top: 1em;
+  margin: 1em;
+}
+</style>
